8a371cade668176cb62670b096bd36140445b6fb
katerina georgiou <katerinaldg@gmail.com>
2017-04-07 13:38:43 +0100

    Returns error message if card is C

--- spec/blackjack_score_spec.rb
+++ spec/blackjack_score_spec.rb
@@ -6,5 +6,10 @@ describe BlackjackScore do
     blackjack_score = BlackjackScore.new
     expect(blackjack_score.get_card_values("A")).to eq("Enter a valid card")
   end
-  
+
+  it "returns 0 if card is C" do
+    blackjack_score = BlackjackScore.new
+    expect(blackjack_score.get_card_values("C")).to eq("Enter a valid card")
+  end
+
 end
