6ee602384303dc54ad0a88791e09fd13a584f610
katerina georgiou <katerinaldg@gmail.com>
2017-04-12 14:11:50 +0100

    Added new file name changes to master branch

---
+++ lib/hand.rb
@@ -0,0 +1,22 @@
+class Hand
+
+  def find_winner(hand1, hand2)
+    if hand1 == hand2
+      "It's a draw!"
+    elsif
+       hand1 > hand2
+      "Hand1 is the winner!"
+    else
+      "Hand2 is the winner!"
+    end
+  end
+
+  def is_out?(hand)
+    if hand > 21
+      true
+    else
+      false
+    end
+  end
+
+end
---
+++ spec/hand_spec.rb
@@ -0,0 +1,30 @@
+require 'hand'
+
+describe Hand do
+
+  it "returns draw if hand1 == hand2" do
+    hand = Hand.new
+    expect(hand.find_winner(0, 0)).to eq("It's a draw!")
+  end
+
+  it "returns winner if hand1 > hand2" do
+    hand = Hand.new
+    expect(hand.find_winner(1, 0)).to eq("Hand1 is the winner!")
+  end
+
+  it "returns winner if hand2 > hand1" do
+    hand = Hand.new
+    expect(hand.find_winner(0, 1)).to eq("Hand2 is the winner!")
+  end
+
+  it "returns false if sum of hand is 0" do
+    hand = Hand.new
+    expect(hand.is_out?(0)).to eq(false)
+  end
+
+  it "returns true if sum of hand is > 21" do
+    hand = Hand.new
+    expect(hand.is_out?(22)).to eq(true)
+  end
+
+end
