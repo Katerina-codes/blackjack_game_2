08b4a62e7c62eaa89bd429ba8ea9f19d515b5b14
katerina georgiou <katerinaldg@gmail.com>
2017-04-12 11:21:42 +0100

    Corrected test name

--- spec/winner_spec.rb
+++ spec/winner_spec.rb
@@ -2,7 +2,7 @@ require 'winner'
 
 describe Winner do
 
-  it "returns winner if hand1 > hand2" do
+  it "returns winner if hand1 == hand2" do
     winner = Winner.new
     expect(winner.find_winner(0, 0)).to eq("It's a draw!")
   end
