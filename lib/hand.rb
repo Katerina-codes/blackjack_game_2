b99d32a02ee64c5da6d140cc73c65c20a50d14f4
Katerina <katerinaldg@gmail.com>
2017-04-22 11:45:46 +0100

    Changed method to see which is the winning hand

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,17 +1,17 @@
 class Hand
 
-  def find_winner(hand1, hand2)
-    if hand1 == hand2
-      "It's a draw!"
-    elsif hand1 > hand2
-      "Hand1 is the winner!"
-    else
-      "Hand2 is the winner!"
-    end
-  end
+	def find_winner(hand1, hand2)
+		if hand1 == hand2
+			"It's a draw!"
+		elsif hand1 > hand2
+			"Hand1 is the winner!"
+		else
+			"Hand2 is the winner!"
+		end
+	end
 
-  def is_out?(hand)
-    hand > 21
-  end
+	def is_out?(hand)
+		hand > 21
+	end
 
 end
