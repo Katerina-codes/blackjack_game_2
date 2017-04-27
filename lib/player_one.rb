23b8238b25cbf1926dba9bc3a73a251d0c6cfed2
Katerina <katerinaldg@gmail.com>
2017-04-27 14:52:14 +0100

    New player class and initial hand method

--- lib/player_one.rb
+++ lib/player_one.rb
@@ -1,6 +1,25 @@
+require_relative 'dealer'
+require_relative 'blackjack_score'
+
 class PlayerOne
 
+  def initialize(output = $stdout)
+    @output = output
+  end
 
-
+  def get_initial_hand
+    Dealer.new.ask_how_many_players
+		Dealer.new.get_num_of_players
+		card1 = Dealer.new.give_new_card
+		card2 = Dealer.new.give_new_card
+		hand = card1 + card2
+		p "Player 1's hand:"
+    p hand
+		hand_value = BlackjackScore.new.get_hand_value(hand)
+    @output
+  end
 
 end
+
+x = PlayerOne.new
+x.get_initial_hand
--- spec/player_one_spec.rb
+++ spec/player_one_spec.rb
@@ -2,6 +2,11 @@ require 'player_one'
 
 describe PlayerOne do
 
-
+ # it "returns two cards for initial hand" do
+ #   output = StringIO.new("[1D, 1D]")
+ #   player_one = PlayerOne.new
+ #   player_one.get_initial_hand
+ #   expect(output.string).to eq("[1D, 1D]")
+ # end
 
 end
