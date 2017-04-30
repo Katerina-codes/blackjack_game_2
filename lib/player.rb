0c67bfe1b3e0d05493c60bec43325ee6578be2d2
Katerina <katerinaldg@gmail.com>
2017-04-30 19:13:55 +0100

    Changed class name so multiple players can be created from one file

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,7 @@
 require_relative 'dealer'
 require_relative 'blackjack_score'
 
-class PlayerOne
-
-  def initialize(output = $stdout)
-    @output = output
-  end
+class Player
 
   def get_initial_hand
     Dealer.new.ask_how_many_players
@@ -16,10 +12,9 @@ class PlayerOne
 		p "Player 1's hand:"
     p hand
 		hand_value = BlackjackScore.new.get_hand_value(hand)
-    @output
   end
 
 end
 
-x = PlayerOne.new
+x = Player.new
 x.get_initial_hand
