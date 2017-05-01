0e22836fbebb0b09f8f42e23842c185616019b97
Katerina <katerinaldg@gmail.com>
2017-05-01 11:41:35 +0100

    Added code for another play in the runner

--- blackjack_game_runner.rb
+++ blackjack_game_runner.rb
@@ -1,9 +1,16 @@
 require_relative 'lib/blackjack_score'
 require_relative 'lib/dealer'
 require_relative 'lib/hand'
+require_relative 'lib/player'
 
 
 x = Dealer.new
-hand = x.deal_initial_hand
-hand_value = x.get_hand_value(hand)
-x.dealer_game_flow(hand, hand_value)
+p player_one = Player.new.get_initial_hand
+p player_two = Player.new.get_initial_hand
+player_one_hand = player_one
+player_one_hand_value = x.get_hand_value(player_one_hand)
+x.dealer_game_flow(player_one_hand, player_one_hand_value)
+
+player_two_hand = player_two
+player_two_hand_value = x.get_hand_value(player_two_hand)
+x.dealer_game_flow(player_two_hand, player_two_hand_value)
