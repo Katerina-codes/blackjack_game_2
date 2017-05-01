eaf3ea08b0462dfc0b53cc79a2942d978c07b41b
Katerina <katerinaldg@gmail.com>
2017-05-01 11:37:48 +0100

    Simplified and renamed get_initial_hand method

--- lib/player.rb
+++ lib/player.rb
@@ -4,17 +4,8 @@ require_relative 'blackjack_score'
 class Player
 
   def get_initial_hand
-    Dealer.new.ask_how_many_players
-		Dealer.new.get_num_of_players
-		card1 = Dealer.new.give_new_card
-		card2 = Dealer.new.give_new_card
-		hand = card1 + card2
-		p "Player 1's hand:"
-    p hand
-		hand_value = BlackjackScore.new.get_hand_value(hand)
+    dealer = Dealer.new
+  player_one_hand = dealer.deal_initial_hand
   end
 
 end
-
-x = Player.new
-x.get_initial_hand
