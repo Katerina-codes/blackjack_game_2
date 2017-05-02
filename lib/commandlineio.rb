871ace41576f118019ca1e15b6563866a9ecbb91
Katerina <katerinaldg@gmail.com>
2017-05-02 17:00:12 +0100

    Got rid of extra spaces in strings

--- lib/commandlineio.rb
+++ lib/commandlineio.rb
@@ -14,7 +14,7 @@ class CommandlineIO
   def get_num_of_players
     num_of_players = @input.gets.chomp.to_i
     until num_of_players == 1 || num_of_players == 2
-      @output.puts " Please enter 1 or 2"
+      @output.puts "Please enter 1 or 2"
       num_of_players = @input.gets.chomp.to_i
     end
     num_of_players
@@ -25,13 +25,13 @@ class CommandlineIO
 	end
 
   def ask_hit_or_stop
-    @output.puts " Do you want to hit or stop?"
+    @output.puts "Do you want to hit or stop?"
   end
 
   def get_hit_or_stop
     user_input = @input.gets.chomp.downcase
     until user_input == "hit" || user_input == "stop"
-      puts " Please enter hit or stop: "
+      puts "Please enter hit or stop: "
       user_input = @input.gets.chomp.downcase
     end
     user_input
@@ -42,7 +42,7 @@ class CommandlineIO
   end
 
   def return_score(hand_value)
-    @output.puts " Game over and your score is: " + hand_value.to_s
+    @output.puts "Game over and your score is: " + hand_value.to_s
   end
 
 end
