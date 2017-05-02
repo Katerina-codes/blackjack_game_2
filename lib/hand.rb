4eed167018e52d12e2f85364e7d06062dba3f61f
Katerina <katerinaldg@gmail.com>
2017-05-02 17:09:19 +0100

    Removed extra condition on find_winner method as whole method needs to be refined

--- lib/hand.rb
+++ lib/hand.rb
@@ -3,7 +3,7 @@ class Hand
 	def find_winner(hand1, hand2)
 		if hand1 == hand2
 			"It's a draw!"
-		elsif hand1 > hand2 && hand1 <= 21
+		elsif hand1 > hand2
 			"Hand1 is the winner!"
 		else
 			"Hand2 is the winner!"
@@ -11,7 +11,7 @@ class Hand
 	end
 
 	def is_out?(hand_value)
-		hand_value.to_i > 21
+		hand_value.to_i >= 21
 	end
 
 end
