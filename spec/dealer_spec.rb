710af393a75c753472f06123f6348e109f4bcc6f
Katerina <katerinaldg@gmail.com>
2017-05-03 10:48:32 +0100

    Corrected test name to make it more clear

--- spec/dealer_spec.rb
+++ spec/dealer_spec.rb
@@ -9,7 +9,7 @@ describe Dealer do
 		expect(dealer.determine_number_of_players).to eq(1)
 	end
 
-	it "returns the current hand plus new card" do
+	it "returns the current hand plus new card '3C'" do
 		dealer = Dealer.new
 		expect(dealer.hand_plus_new_card(["AH", "2D"], ["3C"])).to eq(["AH", "2D", "3C"])
 	end
