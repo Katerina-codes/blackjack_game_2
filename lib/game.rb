bbb9d04823d140e555f72c468c3ead50e3a22549
Katerina <katerinaldg@gmail.com>
2017-05-05 10:43:57 +0200

    Tidied up Game class, added find winner method

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 require_relative "commandlineio"
 require_relative "dealer"
 require_relative "blackjack_score"
+require_relative "hand"
 
 class Game
 
@@ -22,49 +23,25 @@ class Game
 
     hand_played_until_end = @dealer.get_final_hand_value(player_hand)  # should hand or score be getting this?
     @io.return_score(hand_played_until_end)
+    hand_played_until_end
   end
 
   def game_flow
     @io = CommandlineIO.new
+    @hand = Hand.new
 
     @io.ask_how_many_players
     if @io.get_num_of_players == 1
       player_hand = initial_hand
       hand_played_until_end(player_hand)
+    else
+      player_one_hand = initial_hand
+      player_two_hand = initial_hand
+      player_one_hand_result = hand_played_until_end(player_one_hand)
+      print "Player two: "
+      player_two_hand_result = hand_played_until_end(player_two_hand)
+      p @hand.find_winner(player_one_hand_result, player_two_hand_result)
     end
   end
-  # def game_flow
-  #   @io = CommandlineIO.new
-  #   @dealer = Dealer.new
-  #
-  # 	@io.ask_how_many_players
-  # 	if @io.get_num_of_players == 1
-  # 		player_one_hand = Player.new.get_initial_hand
-  # 		@io.display_initial_hand(player_one_hand)
-  # 		player_one_hand_value = @io.get_hand_value(player_one_hand)
-  # 		@io.display_hand_value(player_one_hand_value)
-  #     player_one_final_hand = @dealer.get_final_hand_value(player_one_hand, player_one_hand_value)
-  # 		@io.return_score(player_one_final_hand)
-  #   else
-  # 		player_one_hand = Player.new.get_initial_hand
-  # 		@io.display_initial_hand(player_one_hand)
-  # 		player_one_hand_value = @io.get_hand_value(player_one_hand)
-  # 		@io.display_hand_value(player_one_hand_value)
-  #
-  # 		player_two_hand = Player.new.get_initial_hand
-  # 		@io.display_initial_hand(player_two_hand)
-  # 		player_two_hand_value = @io.get_hand_value(player_two_hand)
-  # 		@io.display_hand_value(player_two_hand_value)
-  #
-  # 		player_two_hand_value = @io.get_hand_value(player_two_hand)
-  # 		player_one_final_hand = @dealer.get_final_hand_value(player_one_hand, player_one_hand_value)
-  # 		@io.return_score(player_one_final_hand)
-  #
-  # 		player_two_hand_value = @io.get_hand_value(player_two_hand)
-  # 		player_two_final_hand = @dealer.get_final_hand_value(player_two_hand, player_two_hand_value)
-  # 		@io.return_score(player_two_final_hand)
-  # 		# p Hand.new.find_winner(player_one_final_hand, player_two_final_hand)
-  # 	end
-  # end
 
 end
