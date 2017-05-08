5eb4d17ca11c536a4863d0ca3904335ac60c6fe8
Katerina <katerinaldg@gmail.com>
2017-05-08 16:20:37 +0100

    Removed test duplication

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -2,9 +2,14 @@ require 'computer'
 
 describe Computer do
 
+  subject(:computer) { described_class.new }
+
   it "returns hit" do
-    computer = Computer.new
-    expect(computer.play_move(["Hit"])).to eq("hit")
+    expect(subject.play_move(["Hit"])).to eq("hit")
+  end
+
+  it "returns stop" do
+    expect(subject.play_move(["stop", "hit"])).to eq("stop")
   end
 
 end
