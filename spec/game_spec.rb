6d9794aeb17c939a6bb15d24d254cfaff68829da
Katerina <katerinaldg@gmail.com>
2017-05-23 06:48:57 +0100

    refactor game tests to remove duplication

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,15 +9,15 @@ class DummyDealer
 end
 
 describe Game do
+  let(:input) { StringIO.new }
+  let(:output) { StringIO.new }
+  let(:io) { CommandlineIO.new(output, input) }
+  let(:dealer) { DummyDealer.new }
+  let(:blackjack_score) { BlackjackScore.new }
+  let(:player) { Player.new }
+  let(:hand) { Hand.new }
 
   it "display the initial hand and value" do
-    input = StringIO.new
-    output = StringIO.new
-    io = CommandlineIO.new(output, input)
-    dealer = DummyDealer.new
-    blackjack_score = BlackjackScore.new
-    player = Player.new
-    hand = Hand.new
     game = Game.new(io, dealer, blackjack_score, player, hand)
 
     game.deal_initial_hand
@@ -26,13 +26,6 @@ describe Game do
   end
 
   it "returns the initial player hand" do
-    input = StringIO.new
-    output = StringIO.new
-    io = CommandlineIO.new(output, input)
-    dealer = DummyDealer.new
-    blackjack_score = BlackjackScore.new
-    player = Player.new
-    hand = Hand.new
     game = Game.new(io, dealer, blackjack_score, player, hand)
 
     expect(game.deal_initial_hand).to eq(["3D", "AS"])
