8f6bc286c6e09c02df0a76358f12b24ad1e78635
Katerina <katerinaldg@gmail.com>
2017-05-23 06:42:25 +0100

    Returns the initial player hand

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -24,4 +24,20 @@ describe Game do
 
     expect(output.string).to include("3D", "AS", "Your hand value is: 14")
   end
+
+  it "returns the initial player hand" do
+    input = StringIO.new
+    output = StringIO.new
+    io = CommandlineIO.new(output, input)
+    dealer = DummyDealer.new
+    blackjack_score = BlackjackScore.new
+    player = Player.new
+    hand = Hand.new
+    game = Game.new(io, dealer, blackjack_score, player, hand)
+
+    expect(game.deal_initial_hand).to eq(["3D", "AS"])
+  end
+
+
+
 end
